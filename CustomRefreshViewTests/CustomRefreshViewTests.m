d628778dd728b6d1ea00d9a7c4ae93b0c86a3f4d
周祥 <zhoux@zzkqdc.com>
2020-09-29 14:00:36 +0800

    Initial Commit

---
+++ CustomRefreshViewTests/CustomRefreshViewTests.m
@@ -0,0 +1,37 @@
+//
+//  CustomRefreshViewTests.m
+//  CustomRefreshViewTests
+//
+//  Created by zhouxiang on 2020/9/29.
+//  Copyright © 2020 zhouxiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomRefreshViewTests : XCTestCase
+
+@end
+
+@implementation CustomRefreshViewTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
